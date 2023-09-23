c77764919c007c4125f5667f86bc8214cf671c2d
Groza Nadezhda <unrip512@gmail.com>
2023-09-23 22:49:05 +0300

    the bash-script bs runs the file main.py on the server and outputs the response in the local terminal

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import numpy as np
-
-p = int(input("please, enter the size of the square matrix: "))
-
-if (p > 5):
-    print("the number is more then 5")
-else:
-    print("the number is less then or equal to 5")
-
-a = np.zeros((p,p))
-print(a)
-print(type(a))
-
+print("Hello world!")
