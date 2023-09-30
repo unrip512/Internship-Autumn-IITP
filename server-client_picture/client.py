33566f9b17a8eceb43fd80724e22753862d3d141
Groza Nadezhda <unrip512@gmail.com>
2023-09-30 21:49:31 +0300

    .

--- server-client_picture/client.py
+++ server-client_picture/client.py
@@ -6,8 +6,8 @@ client_socket = socket.socket()
 client_socket.connect(('10.0.111.233', 2349))
 
 f = open('got_image.jpg', mode='wb')
-data = client_socket.recv(2048)
 
+data = client_socket.recv(2048)
 while data:
     f.write(data)
     data = client_socket.recv(2048)
--- server-client_picture/server.py
+++ server-client_picture/server.py
@@ -11,11 +11,11 @@ print("Сервер запущен и ожидает подключений...")
 client_socket, client_address = server_socket.accept()
 print(f"Клиент подключился: {client_address}")
 
+
 pict = 'photo.jpg'
-
 f = open(pict, mode='rb')
-data1 = f.read(2048)
 
+data1 = f.read(2048)
 while data1:
     client_socket.send(data1)
     data1 = f.read(2048)
