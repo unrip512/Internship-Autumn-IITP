5b1a466036e24a7e43de2985ea142621edd135c6
Groza Nadezhda <unrip512@gmail.com>
2023-09-30 20:11:03 +0300

    a picture is transmitted to the client, but small

--- time-client/client.py
+++ time-client/client.py
@@ -1,9 +1,16 @@
 import time
 import socket
+from base64 import b64decode as dec
+from base64 import b64encode as enc
+from io import BytesIO
+from PIL import Image
 
-num = 5
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-time.sleep(1)
+num = 1
+
+time.sleep(5)
 client_socket = socket.socket()
 client_socket.connect(('10.0.111.233', 2348))
 
@@ -12,5 +19,10 @@ client_socket.send((str(num)).encode())
 for i in range(num):
     data = client_socket.recv(1024)
     print(f'Получено: {data.decode()}')
+
+by_pic = client_socket.recv(30000)
+pic = BytesIO(dec(by_pic))
+pillow = Image.open(pic)
+y = Image._show(pillow)
 
 client_socket.close()
--- time-client/server.py
+++ time-client/server.py
@@ -1,5 +1,8 @@
 import socket
 import time
+from base64 import b64encode as enc
+import sys
+
 server_socket = socket.socket()
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -11,13 +14,18 @@ print("Сервер запущен и ожидает подключений...")
 client_socket, client_address = server_socket.accept()
 print(f"Клиент подключился: {client_address}")
 
-num = client_socket.recv(1024)
+num = client_socket.recv(3000)
 print(f"Клиент отправил данные: {num}, {type(num)}")
 num = int(num.decode())
-
 
 for i in range(num):
     client_socket.send((time.ctime(time.time())).encode())
     time.sleep(5)
+
+pict = 'photo3.jpg'
+with open(pict, 'rb') as f:
+    by_pict = enc(f.read())
+    print(sys.getsizeof(by_pict))
+    client_socket.send(by_pict)
 
 client_socket.close()
