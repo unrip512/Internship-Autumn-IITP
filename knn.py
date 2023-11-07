c642ff994a18c3c16574a947340d2ef3e6008da3
Groza Nadezhda <96203040+unrip512@users.noreply.github.com>
2023-11-07 21:03:04 +0300

    Update knn.py

--- knn.py
+++ knn.py
@@ -2,14 +2,14 @@ import numpy as np
 from collections import Counter
 
 
-def L2_norm(x, y):                       #функция считает Евклидово расстояние между двумя точками
+def L2_norm(x, y):                                                   #функция считает Евклидово расстояние между двумя точками
     return np.linalg.norm(x-y, ord=2)
 
 
 class Point:
 
-    value_vector = None              #координаты точки
-    ob_class = None                  #класс, к которому точка принадлежит
+    value_vector = None                                              #координаты точки
+    ob_class = None                                                  #класс, к которому точка принадлежит
 
     def __init__(self, value, class_):
         self.value_vector = np.array(value)
@@ -17,25 +17,25 @@ class Point:
 
 class Knn:
 
-    tr_set = []                 #здесь будет храниться обучающая выборка
-    k = None                    #колличество соседей
+    tr_set = []                                                      #здесь будет храниться обучающая выборка
+    k = None                                                         #колличество соседей
 
     def __init__(self, num_of_ngb):
         self.k = num_of_ngb
 
-    def fit(self, x_val, y_val):        #принимает массив точек и массив классов, которым эти точки соответствуют
+    def fit(self, x_val, y_val):                                    #принимает массив точек и массив классов, которым эти точки соответствуют
         num_of_points = len(y_val)
 
-        for i in range(num_of_points):  #заполняет массив tr_set
+        for i in range(num_of_points):                              #заполняет массив tr_set
             self.tr_set.append(Point(x_val[i], y_val[i]))
 
     def predict(self, x_val):
-        distance = sorted(self.tr_set, key=lambda item: L2_norm(x_val, item.value_vector))  #сортирует точки в зависимости от расстояния до заданной
-        distance = distance[:self.k]        #оставляет только первые k из них
+        distance = sorted(self.tr_set, key=lambda item: L2_norm(x_val, item.value_vector))                      #сортирует точки в зависимости от расстояния до заданной
+        distance = distance[:self.k]                                                                            #оставляет только первые k из них
 
-        class_variety = [item.ob_class for item in distance]  #массив из значений классов, которым принадлежат ближайшие k соседей
+        class_variety = [item.ob_class for item in distance]                                                    #массив из значений классов, которым принадлежат ближайшие k соседей
 
-        return Counter(class_variety).most_common(1)[0][0]  # возвращает класс, который встречался наибольшее число раз в массиве class_variety
+        return Counter(class_variety).most_common(1)[0][0]                                                      # возвращает класс, который встречался наибольшее число раз в массиве class_variety
 
 
 points = np.array([[2, 2], [1, 1], [3, 1], [6, 5], [6, 4], [5, 3]])
